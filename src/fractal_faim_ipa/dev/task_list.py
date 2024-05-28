569dad3b0ee14620392ae899c9d5bb3e765f2dc8
jluethi <joel.luethi@uzh.ch>
2024-05-28 19:08:20 +0200

    Rename repo to fractal-faim-ipa

---
+++ src/fractal_faim_ipa/dev/task_list.py
@@ -0,0 +1,11 @@
+"""Fractal Task list for Fractal Helper Tasks."""
+
+from fractal_tasks_core.dev.task_models import NonParallelTask, ParallelTask
+
+TASK_LIST = [
+    NonParallelTask(
+        name="FAIM-IPA Convert MD to OME-Zarr",
+        executable="md_create_ome_zarr.py",
+        meta={"cpus_per_task": 8, "mem": 32000},
+    ),
+]
