61372f78c14c19bdc38aee02c20aca776af07960
jluethi <joel.luethi@uzh.ch>
2024-05-29 13:37:25 +0200

    Update manifest task name & tests to use V2 manifests

--- src/fractal_faim_ipa/dev/task_list.py
+++ src/fractal_faim_ipa/dev/task_list.py
@@ -4,7 +4,7 @@ from fractal_tasks_core.dev.task_models import NonParallelTask
 
 TASK_LIST = [
     NonParallelTask(
-        name="FAIM-IPA Convert MD to OME-Zarr",
+        name="Convert MD to OME-Zarr",
         executable="md_create_ome_zarr.py",
         meta={"cpus_per_task": 8, "mem": 32000},
     ),
--- tests/test_valid_manifest.py
+++ tests/test_valid_manifest.py
@@ -1,33 +1,32 @@
 import json
-import sys
 from pathlib import Path
 
 import fractal_tasks_core
 import requests
-from devtools import debug
+from jsonschema import validate
 
 
 def test_valid_manifest(tmp_path):
     """
     NOTE: to avoid adding a fractal-server dependency, we simply download the
-    relevant file. In the future we may have a fractal-common package, and that
-    one could be easily added as a `dev` dependency.
+    relevant file.
     """
-
+    # Download JSON Schema for ManifestV2
     url = (
         "https://raw.githubusercontent.com/fractal-analytics-platform/"
-        "fractal-common/main/schemas/manifest.py"
+        "fractal-server/main/"
+        "fractal_server/json_schemas/manifest_v2.json"
     )
     r = requests.get(url)
-    debug(tmp_path)
-    with (tmp_path / "fractal_manifest.py").open("wb") as fout:
-        fout.write(r.content)
+    with (tmp_path / "manifest_schema_v2.json").open("wb") as f:
+        f.write(r.content)
+    with (tmp_path / "manifest_schema_v2.json").open("r") as f:
+        manifest_schema = json.load(f)
 
-    sys.path.append(str(tmp_path))
-    from fractal_manifest import ManifestV1
-
+    # Load tasks-package manifest
     module_dir = Path(fractal_tasks_core.__file__).parent
     with (module_dir / "__FRACTAL_MANIFEST__.json").open("r") as fin:
         manifest_dict = json.load(fin)
-    manifest = ManifestV1(**manifest_dict)
-    debug(manifest)
+
+    # Validate manifest against JSON Schema
+    validate(instance=manifest_dict, schema=manifest_schema)
