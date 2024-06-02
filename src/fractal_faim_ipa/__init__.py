58441d70f8ad704a66f019ca0145b7e003279d55
jluethi <joel.luethi@uzh.ch>
2024-06-02 22:46:14 +0200

    Cleanup

--- src/fractal_faim_ipa/__init__.py
+++ src/fractal_faim_ipa/__init__.py
@@ -1,9 +1,9 @@
-"""Provides Fractal tasks for the MD to OME-Zarr conversion of faim-hcs."""
+"""Provides Fractal tasks for the MD to OME-Zarr conversion of faim-ipa."""
 
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("fractal-faim-hcs")
+    __version__ = version("fractal-faim-ipa")
 except PackageNotFoundError:
     __version__ = "uninstalled"
 __author__ = "Joel Luethi"
