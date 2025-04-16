32d0ae65993f12a0d87e332c0ead2f7d986fe987
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-04-16 08:35:44 +0200

    Switch to `fractal-task-tools` for manifest building

--- src/fractal_faim_ipa/dev/create_manifest.py
+++
@@ -1,8 +0,0 @@
-"""Generate JSON schemas for tasks and write them to the Fractal manifest."""
-
-from fractal_tasks_core.dev.create_manifest import create_manifest
-
-if __name__ == "__main__":
-    PACKAGE = "fractal_faim_ipa"
-    docs_link = "https://github.com/fractal-analytics-platform/fractal-faim-ipa"
-    create_manifest(package=PACKAGE, docs_link=docs_link)
